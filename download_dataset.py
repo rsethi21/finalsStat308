8fb94c4680f80369e30b495db998268f056aaae4
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-11-13 15:01:20 -0600

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,13 +1,1 @@
-from ucimlrepo import fetch_ucirepo 
-import pandas as pd
 
-# fetch dataset 
-cdc_diabetes_health_indicators = fetch_ucirepo(id=891) 
-  
-# data (as pandas dataframes) 
-X = cdc_diabetes_health_indicators.data.features 
-y = cdc_diabetes_health_indicators.data.targets 
- 
-# save as csv
-X['diabetes'] = y.iloc[:,0]
-X.to_csv("data.csv")
